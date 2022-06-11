6160f2b5f6953f8259148fa1b19c70c5949b8910
Prabal Rawal <prabal@Prabals-MacBook-Air.local>
2022-06-12 02:59:42 +0530

    tables can be created sucessfully

---
+++ tables.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h> 
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+   int i;
+   for(i = 0; i<argc; i++) {
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
+}
+
+int main(int argc, char* argv[]) {
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+   char *sql;
+
+   /* Open database */
+   rc = sqlite3_open("data/students.db", &db);
+   
+   if( rc ) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      return(0);
+   } else {
+      fprintf(stdout, "Opened database successfully\n");
+   }
+
+   /* Create SQL statement */
+   sql = "CREATE TABLE STUDENTS("  \
+      "ID INT PRIMARY KEY     NOT NULL," \
+      "FIRST_NAME    TEXT     NOT NULL," \
+      "LAST_NAME     TEXT     NOT NULL," \
+      "AGE            INT     NOT NULL)";
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+   
+   if( rc != SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   } else {
+      fprintf(stdout, "Table created successfully\n");
+   }
+   sqlite3_close(db);
+   return 0;
+}
+
+
+/*
+CREATE TABLE STUDENTS(
+   ID INT PRIMARY KEY NOT NULL,
+   NAME TEXT NOT NULL,
+   AGE INT NOT NULL,
+   PHONE INT NOT NULL
+);
+
+INSERT INTO STUDENTS(ID, NAME, AGE, PHONE) VALUES(01, akka, 19, 99999999);
+
+*/
